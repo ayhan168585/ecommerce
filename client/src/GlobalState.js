070a95496f611b21947f3754e57f4a6a386022fb
ayhan168585 <49242925+ayhan168585@users.noreply.github.com>
2021-10-28 19:44:36 +0300

    28.10.2021-19:45

---
+++ client/src/GlobalState.js
@@ -0,0 +1,16 @@
+import React,{createContext,useState} from "react";
+import ProductsAPI from "./api/ProductsAPI";
+
+
+export const GlobalState=createContext()
+
+
+
+export const DataProvider=({children})=>{
+    ProductsAPI()
+    return(
+        <GlobalState.Provider value={"Value in Global"}>
+            {children}
+        </GlobalState.Provider>
+    )
+}
