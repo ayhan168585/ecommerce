8d0fcf1ca67991ad7b1a4ee8ff31f3666cc76406
ayhan168585 <49242925+ayhan168585@users.noreply.github.com>
2021-11-01 20:33:24 +0300

    01.11.2021-20:33

--- client/src/components/mainpages/cart/Cart.js
+++ client/src/components/mainpages/cart/Cart.js
@@ -1,11 +1,62 @@
-import React, { useContext,useState } from "react";
+import React, { useContext, useState, useEffect } from "react";
 import { GlobalState } from "../../../GlobalState";
 import { Link } from "react-router-dom";
+import axios from "axios";
 
 function Cart() {
   const state = useContext(GlobalState);
-  const [cart] = state.userAPI.cart;
-  const [total,setTotal]=useState(0)
+  const [cart, setCart] = state.userAPI.cart;
+  const [token]=state.token
+  const [total, setTotal] = useState(0);
+
+  useEffect(() => {
+    const getTotal = () => {
+      const total = cart.reduce((prev, item) => {
+        return prev + item.price * item.quantity;
+      }, 0);
+      setTotal(total);
+    };
+    getTotal();
+  }, [cart]);
+
+  const addToCart=async()=>{
+   
+      await axios.patch("/user/addcart",{cart},{
+        headers:{Authorization: token}
+      })
+   
+  }
+  const increment = (id) => {
+    cart.forEach((item) => {
+      if (item._id === id) {
+        item.quantity += 1;
+      }
+    });
+    setCart([...cart]);
+    addToCart()
+  };
+
+  const decrement = (id) => {
+    cart.forEach((item) => {
+      if (item._id === id) {
+        item.quantity === 1 ? (item.quantity = 1) : (item.quantity -= 1);
+      }
+    });
+    setCart([...cart]);
+    addToCart()
+  };
+
+  const removeProduct = (id) => {
+    if (window.confirm("Bu ürünü sepetten çıkarmak istiyormusunuz?")) {
+      cart.forEach((item, index) => {
+        if (item._id === id) {
+          cart.splice(index, 1);
+        }
+      });
+      setCart([...cart]);
+      addToCart()
+    }
+  };
 
   if (cart.length === 0)
     return (
@@ -14,25 +65,27 @@ function Cart() {
   return (
     <div>
       {cart.map((product) => (
-        <div className="detail cart">
+        <div className="detail cart" key={product._id}>
           <img src={product.images.url} alt="" />
           <div className="box-detail">
-              <h2>{product.title}</h2>
-            <h3>{product.price*product.quantity},00 TL.</h3>
+            <h2>{product.title}</h2>
+            <h3>{product.price * product.quantity},00 TL.</h3>
             <p>{product.description}</p>
             <p>{product.content}</p>
             <div className="amount">
-                <button> - </button>
-                <span>{product.quantity}</span>
-                <button> + </button>
+              <button onClick={() => decrement(product._id)}> - </button>
+              <span>{product.quantity}</span>
+              <button onClick={() => increment(product._id)}> + </button>
             </div>
-          <div className="delete">X</div>
+            <div className="delete" onClick={() => removeProduct(product._id)}>
+              X
+            </div>
           </div>
         </div>
       ))}
       <div className="total">
-          <h3>Toplam : {total}</h3>
-          <Link to="">Ödeme yap</Link>
+        <h3>Toplam : {total},00 TL.</h3>
+        <Link to="">Ödeme yap</Link>
       </div>
     </div>
   );
